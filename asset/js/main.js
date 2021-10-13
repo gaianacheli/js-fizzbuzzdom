ba73c8601fe7f6c647b84965d71166ebf63ef944
gaianacheli <gaianacheli04@gmail.com>
2021-10-13 15:55:20 +0200

    stesura numeri da 1 a 100 con relative condizioni

--- asset/js/main.js
+++ asset/js/main.js
@@ -1,14 +1,20 @@
-//esegua un ciclo da 1 a 100 
-
-
-
-//e a ogni iterazione appenda un elemento html al container con uno stile differente a seconda del valore dell'indice
-//per i multipli di 3, per i multipli di 5 e per i valori che sono sia multipli di 3 che di 5.
+/*esegua un ciclo da 1 a 100 
+e a ogni iterazione appenda un elemento html al container con uno stile differente a seconda del valore dell'indice
+per i multipli di 3, per i multipli di 5 e per i valori che sono sia multipli di 3 che di 5.*/
 
 const list = document.querySelector ('.list')
 
-for (let i= 0;i <= 100; i++){
-    console.log(i)
-    const element = '<li class="box"> ${i} </li>'
-    list.innerHTML += i
+for (let i= 1; i <= 100; i++){
+    if (i % 3 == 0) {
+        document.write('ciao')
+    }else if (i % 5 == 0){
+        document.write('bello')
+    }else if  ((i % 3 == 0 )&& (i % 5 == 0)){
+        document.write('buono')
+
+    }else {
+        document.write(i)
+    }
 }
+    
+   
